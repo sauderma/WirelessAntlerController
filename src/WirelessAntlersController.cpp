e26e613d33b42b7fbf0cfc7db98d12b163d95750
Michael Sauder <michael@shownet.io>
2021-10-18 21:51:36 -0400

    Updaded SafeString, changed structs to use bytes

--- src/WirelessAntlersController.cpp
+++ src/WirelessAntlersController.cpp
@@ -15,15 +15,16 @@
 #include <SPIFlash.h>      // https://github.com/lowpowerlab/spiflash
 #include <SafeString.h>    // https://www.forward.com.au/pfod/ArduinoProgramming/SafeString/index.html
 #include <SafeStringReader.h> // https://www.forward.com.au/pfod/ArduinoProgramming/SafeString/index.html
+#include <DebugLog.h>
 
-#define NODEID       3
+#define NODEID       1
 #define NETWORKID    150
 #define GATEWAY1     1
 #define GATEWAY2     2
 #define GATEWAY3     3
 #define BROADCASTID  0
 #define FREQUENCY     RF69_915MHZ
-#define FREQUENCY_EXACT 915000000
+#define FREQUENCY_EXACT 905500000
 #define ENCRYPTKEY  "rcmhprodrcmhprod" //16-bytes or ""/0/null for no encryption
 #define IS_RFM69HW_HCW  //uncomment only for RFM69HW/HCW! Leave out if you have RFM69W/CW!
 //*****************************************************************************************************************************
@@ -58,26 +59,22 @@ long lastPeriod = -1;
 createSafeStringReader(sfReader, 80, '\n'); // Create reader up to 80 characters for strings coming in via 
                                             // serial with new line character as the delimiter.
 
-//createSafeStringReader()
-
-
-
 // struct for packets being sent to antler hats
 typedef struct {
-  int  nodeId; // Sender node ID
-  int  version; // What version payload
-  int  nodeState; // What state should node go into?
-  int  antlerState; // What state should the actual antlers to be in?
+  byte  nodeId; // Sender node ID
+  byte  version; // What version payload
+  byte  nodeState; // What state should node go into?
+  byte  antlerState; // What state should the actual antlers to be in?
   long sleepTime; // In milliseconds. Used if we want to overwrite pre-defined states
 } ToAntlersPayload;
 //ToAntlersPayload antlersPayload;
 
 // struct for packets being sent to controllers
 typedef struct {
-  int  nodeId; // Sender node ID
-  int  version; // What version payload
-  int  nodeState; // What state Hat node is currently in
-  int  antlerState; // What state the antlers are currently in
+  byte  nodeId; // Sender node ID
+  byte  version; // What version payload
+  byte  nodeState; // What state Hat node is currently in
+  byte  antlerState; // What state the antlers are currently in
   float vcc; // VCC read from battery monitor
   int   temperature; // Temperature of the radio
 } ToControllersPayload;
@@ -115,14 +112,14 @@ void setup() {
   Serial.println(NODEID);
 
   if (flash.initialize())
-    Serial.println("SPI Flash Init OK!");
+    LOG_INFO("SPI Flash Init OK!");
   else
-    Serial.println("SPI Flash Init FAIL!");
+    LOG_WARN("SPI Flash Init FAIL!");
 
-  Serial.println("Listening at 915 Mhz...");
-  Serial.println(FREQUENCY_EXACT);
-  Serial.println(NETWORKID);
-  Serial.println(ENCRYPTKEY);
+  LOG_INFO("Listening at 915 Mhz...");
+  LOG_INFO(FREQUENCY_EXACT);
+  LOG_INFO(NETWORKID);
+  LOG_INFO(ENCRYPTKEY);
 
 #ifdef BR_300KBPS
   radio.writeReg(0x03, 0x00);  //REG_BITRATEMSB: 300kbps (0x006B, see DS p20)
@@ -146,11 +143,9 @@ void setup() {
 void sendAntlerPayload(ToAntlersPayload payload, byte node = 0)
 {
   radio.send(node, (const void*)(&payload), sizeof(payload), false);
+  LOG_INFO("Antler payload sent");
 
-  Serial.println(payload.version);
-  Serial.println(payload.nodeState);
-  Serial.println(payload.antlerState);
-  Serial.println(payload.sleepTime);
+  //radio.send(0, "hi", 2);
 }
 
 //*************************************
@@ -161,41 +156,80 @@ bool parseSerialData (SafeString& msg) {
   ToAntlersPayload antlerPayload; // Struct to populate
   antlerPayload.nodeId = NODEID; // First payload value is the node ID of this controller
 
+  int tempInt;
+  byte tempByte;
+
   int toNode; // Node we're going to be sending to
-  cSF(sfKey, 2); // temp SafeString to receive keys, max field len is 2;
-  cSF(sfValue, 10); // temp SafeString to receive values, max field len is 10;
-  char delimKey[] = ":"; // keys separated from values by :
-  char delimValue[] = ","; // key/value pairs are separated by commas
+  cSF(sfKey, 30); // temp SafeString to receive keys, max field len is 2;
+  //cSF(sfValue, 20); // temp SafeString to receive values, max field len is 10;
+  char delimKey[] = ":,"; // keys separated from values by :
+  //char delimValue[] = ","; // key/value pairs are separated by commas
   //bool returnEmptyFields = true; // return empty field for ,,
   int idx = 2;  // Index holding our location in the message.
                 // Skip the first two characters, which should be a "!!"
 
-  idx = msg.nextToken(sfKey, idx, delimKey); // get the first field, stick it into "sfKey"
-  if (sfKey != "ND")  // Check the first field is "ND" (node ID) otherwise something went wrong.
+  msg.removeBefore(2);
+
+  msg.nextToken(sfKey, delimKey); // get the Node ID header, stick it into "sfKey"
+  LOG_INFO("sfKey Node header is: "); LOG_INFO(sfKey);
+  if (!sfKey.equals("ND")){  // Check the first field is "ND" (node ID) otherwise something went wrong.
+    LOG_WARN("ND didn't pass");
     return false;
-  idx = msg.nextToken(sfValue, idx, delimValue); //Get the Node ID
-  sfValue.toInt(toNode); // First value is the node we're sending to (but that's not part of the payload).
-  
-  idx = msg.nextToken(sfKey, idx, delimKey); // get Version
-  if (sfKey != "VR")
+  }
+  msg.nextToken(sfKey, delimKey); //Get the Node ID value
+  sfKey.toInt(toNode); // First value is the node we're sending to, save for later.
+  LOG_INFO("sfkey Node value is:"); LOG_INFO(toNode);
+
+  msg.nextToken(sfKey, delimKey); // get Version header
+  LOG_INFO("sfKey Version header is: "); LOG_INFO(sfKey);
+  if (!sfKey.equals("VR")) {
+    LOG_WARN("VS didn't pass");
     return false;
-  idx = msg.nextToken(sfValue, idx, delimValue);
-  sfValue.toInt(antlerPayload.version);
-  idx = msg.nextToken(sfKey, idx, delimKey); // get Node state
-  if (sfKey != "NS")
+  }
+  msg.nextToken(sfKey, delimKey); // Get Version value
+  LOG_INFO("sfKey Version value is: "); LOG_INFO(sfKey);
+  //sfKey.toInt(antlerPayload.version);
+  LOG_WARN(antlerPayload.version);
+  sfKey.toInt(tempInt);
+  antlerPayload.version = byte(tempInt);
+
+
+  msg.nextToken(sfKey, delimKey); // get Node state header
+  LOG_INFO("sfKey Node State header is: "); LOG_INFO(sfKey);
+  if (!sfKey.equals("NS")) {
+    LOG_WARN("NS didn't pass");
     return false;
-  idx = msg.nextToken(sfValue, idx, delimValue);
-  sfValue.toInt(antlerPayload.nodeState);
-  idx = msg.nextToken(sfKey, idx, delimKey); // get Antler state
-  if (sfKey != "AS")
+  }
+  msg.nextToken(sfKey, delimKey); // get Node state value
+  LOG_INFO("sfKey Node State value is: "); LOG_INFO(sfKey);
+  //sfKey.toInt(antlerPayload.nodeState);
+  LOG_WARN(antlerPayload.nodeState);
+  sfKey.toInt(tempInt);
+  antlerPayload.nodeState = byte(tempInt);
+
+  msg.nextToken(sfKey, delimKey); // get Antler state header
+  LOG_INFO("sfKey Antler State header is: "); LOG_INFO(sfKey);
+  if (!sfKey.equals("AS")) {
+    LOG_WARN("AS didn't pass");
     return false;
-  idx = msg.nextToken(sfValue, idx, delimValue);
-  sfValue.toInt(antlerPayload.antlerState);
-  idx = msg.nextToken(sfKey, idx, delimKey); // get node sleep time
-  if (sfKey != "SL")
+  }
+  msg.nextToken(sfKey, delimKey); // get Antler state value
+  LOG_INFO("sfKey Antler State header is: "); LOG_INFO(sfKey);
+  //sfKey.toInt(antlerPayload.antlerState);
+  LOG_WARN(antlerPayload.antlerState);
+  sfKey.toInt(tempInt);
+  antlerPayload.antlerState = byte(tempInt);
+
+  msg.nextToken(sfKey, delimKey); // get node sleep time header
+  LOG_INFO("sfKey Time header is: "); LOG_INFO(sfKey);
+  if (!sfKey.equals("SL")) {
+    LOG_WARN("SL didn't pass");
     return false;
-  idx = msg.nextToken(sfValue, idx, delimValue);
-  sfValue.toLong(antlerPayload.sleepTime);
+  }
+  msg.nextToken(sfKey, delimKey); // get node sleep time value
+  LOG_INFO("sfKey Time value is: "); LOG_INFO(sfKey);
+  sfKey.toLong(antlerPayload.sleepTime);
+  LOG_WARN(antlerPayload.sleepTime);
 
   // Send all the above stuff over to the nodes
   sendAntlerPayload(antlerPayload, toNode);
@@ -211,16 +245,7 @@ bool parseSerialData (SafeString& msg) {
 
 void blinkLED(int blinkTime, int blinkNumber)
 {
-  // This is terrible don't do this, it blocks the whole thing.
 
-  // int counter = 0;
-  // while (counter <= blinkNumber) {
-  //   digitalWrite(LED_BUILTIN, HIGH);
-  //   delay(blinkTime);
-  //   digitalWrite(LED_BUILTIN, LOW);
-  //   delay(blinkTime);
-  //   counter++;
-  // }
 }
 
 //*****************************************************************************
@@ -229,17 +254,6 @@ void blinkLED(int blinkTime, int blinkNumber)
 
 void loop(){
 
-    // Handle serial input
-    // if (Serial.available() > 0) {
-    //   input = Serial.parseInt();
-    //   //int intInput = input - '0'
-
-    //   if (input >= 1 && input <= 9) { //0-9
-    //     Serial.print("\nSending state "); Serial.println(input);
-    //     sendAntlerPayload((byte)input, 0, 0, 0, 0);
-    //   } 
-    // }  // close if Serial.available()
-
   // sfReader is our serial port handler, check for incoming serial data
   if (sfReader.read()) {
     sfReader.trim(); // remove any leading/trailing white space
@@ -247,7 +261,7 @@ void loop(){
         parseSerialData(sfReader); // Process incoming data string
       }
       else {
-        // process something else
+        LOG_WARN("Incoming serial packet did not start with !!");
       }
     }
   
@@ -256,24 +270,23 @@ void loop(){
 
     if (radio.ACKRequested()) {
       radio.sendACK();
-      #ifdef DEBUG_MODE
-        Serial.println(" - ACK sent");
-      #endif
+      LOG_INFO("Radio ACK sent");
     }
     
     // Check for a new OTA sketch. If so, update will be applied and unit restarted.
-    CheckForWirelessHEX(radio, flash, false);
+    // Disabled for the controllers.
+    // CheckForWirelessHEX(radio, flash, false);
 
-   #ifdef DEBUG_MODE
-      Serial.print("Got [");
-      Serial.print(radio.SENDERID);
-      Serial.print(':');
-      Serial.print(radio.DATALEN);
-      Serial.print("] > ");
-      for (byte i = 0; i < radio.DATALEN; i++)
-        Serial.print((char)radio.DATA[i], HEX);
-      Serial.println();
-    #endif
+  //  #ifdef DEBUG_MODE
+  //     Serial.print("Got [");
+  //     Serial.print(radio.SENDERID);
+  //     Serial.print(':');
+  //     Serial.print(radio.DATALEN);
+  //     Serial.print("] > ");
+  //     for (byte i = 0; i < radio.DATALEN; i++)
+  //       Serial.print((char)radio.DATA[i], HEX);
+  //     Serial.println();
+  //   #endif
 
     // Check if valid packet. In future perhaps add checking for different payload versions
 //    if (radio.DATALEN != sizeof(ToControllersPayload)) {
@@ -286,18 +299,14 @@ void loop(){
       controllersPayload = *(ToControllersPayload*)radio.DATA; // We'll hope radio.DATA actually contains our struct and not something else
 
       //Send the data straight out the serial, we don't actually need to do anything with it internally
-      Serial.print("ID:");Serial.println(controllersPayload.nodeId);      // Node ID
-      Serial.print("VR:");Serial.println(controllersPayload.version);     // Payload version
-      Serial.print("NS:");Serial.println(controllersPayload.nodeState);       // Node state
-      Serial.print("AS:");Serial.println(controllersPayload.antlerState); // Antler state
-      Serial.print("VC:");Serial.println(controllersPayload.vcc);         // Battery voltage
+      Serial.print("##");
+      Serial.print("ID:");Serial.print(controllersPayload.nodeId);Serial.print(",");      // Node ID
+      Serial.print("VR:");Serial.print(controllersPayload.version);Serial.print(",");     // Payload version
+      Serial.print("NS:");Serial.print(controllersPayload.nodeState);Serial.print(",");       // Node state
+      Serial.print("AS:");Serial.print(controllersPayload.antlerState);Serial.print(","); // Antler state
+      Serial.print("VC:");Serial.print(controllersPayload.vcc);Serial.print(",");         // Battery voltage
       Serial.print("TP:");Serial.println(controllersPayload.temperature); // Radio temperature
     
     //} // close valid payload
   } // close radio.receiveDone()
 } // close loop()
-
-
-!!ID:1,VR:1,NS:1,AS:0
-
-001_01_110
